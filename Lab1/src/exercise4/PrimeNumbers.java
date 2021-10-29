8743536785807dc642b3937d54e359002e0f151f
neelamsaikrishna <92876597+neelamsaikrishna@users.noreply.github.com>
2021-10-29 10:03:46 +0530

    Add files via upload

---
+++ Lab1/src/exercise4/PrimeNumbers.java
@@ -0,0 +1,25 @@
+package exercise4;
+
+import java.util.Scanner;
+
+public class PrimeNumbers {
+
+	public static void main(String[] args) {
+		int n;
+		int p;
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter number: ");
+		n = s.nextInt();
+		for (int i = 2; i <= n; i++) {
+			p = 0;
+			for (int j = 2; j < i; j++) {
+				if (i % j == 0)
+					p = 1;
+			}
+			if (p == 0)
+				System.out.println(i);
+		}
+
+	}
+
+}
